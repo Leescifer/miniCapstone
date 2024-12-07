d5450c487a66b710555501dce6db72c7146319e6
Leecifer <128757198+Leescifer@users.noreply.github.com>
2024-12-07 12:48:15 +0800

    Updates

--- src/js/landingPage.js
+++ src/js/landingPage.js
@@ -1,8 +1,25 @@
-//Navbar Menu
 const menuToggle = document.getElementById('menu-toggle');
-const mobileMenu = document.getElementById('mobile-menu');
+const sidebar = document.getElementById('sidebar');
+const backdrop = document.getElementById('backdrop');
+const closeMenu = document.getElementById('close-menu');
 
+// Open sidebar on mobile
 menuToggle.addEventListener('click', () => {
-  mobileMenu.classList.toggle('hidden');
+    sidebar.classList.remove('-translate-x-full');
+    sidebar.classList.add('translate-x-0');
+    backdrop.classList.remove('hidden');
 });
 
+// Close sidebar
+closeMenu.addEventListener('click', () => {
+    sidebar.classList.remove('translate-x-0');
+    sidebar.classList.add('-translate-x-full');
+    backdrop.classList.add('hidden');
+});
+
+// Close sidebar when clicking on backdrop
+backdrop.addEventListener('click', () => {
+    sidebar.classList.remove('translate-x-0');
+    sidebar.classList.add('-translate-x-full');
+    backdrop.classList.add('hidden');
+});
